29ac08481e3d1160255977902df546ce3005c1cb
Nelson Ardila Rojas <nelsneider@gmail.com>
2023-03-30 14:42:18 -0500

    Se ajusta variable de entorno para prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
 };
 
-//export const API_URL = 'https://cartago.syspotec.co:6060/cartago/servig';
-export const API_URL = 'https://desarrollo.syspotec.co:8081/ords/cartapru/servig';
+export const API_URL = 'https://cartago.syspotec.co:6060/cartago/servig';
+//export const API_URL = 'https://desarrollo.syspotec.co:8081/ords/cartapru/servig';
 export const VERSION = 202303;
